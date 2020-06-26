b9d3e15ad78450c1fc852e332d494532a74ab960
Farhan Karim <farhankarim507@gmail.com>
2020-06-26 10:49:58 +0500

    init

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
 # flask library get Flask get 
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,redirect,session
 from math import sqrt,floor
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired,Length,Email
+from flask_bootstrap import Bootstrap
+
+
+class FeedbackForm(FlaskForm):
+    name=StringField('Name',validators=[DataRequired(),Length(min=2,max=50,message="Name should be between 2 and 50 characters long")])
+    email=StringField('Email',validators=[DataRequired(),Email(message="Email format is incorrect"),
+    Length(min=2,max=50,message="Name should be between 2 and 50 characters long")])
+    text=StringField('Text',validators=[DataRequired()])
+    submit=SubmitField("Submit",validators=[DataRequired()])
 
 app=Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY']='5gfg54g45tg4g5sdasdsa4g45g'
 
 #create a route with name calculator
 #a,b
 #add,sub,mul,div
 #return results in string format
+
+@app.route('/feedback',methods=('GET','POST'))
+def feedback():
+    feedback=FeedbackForm()
+    if feedback.validate_on_submit():
+        session['name']=feedback.name.data
+        session['email']=feedback.email.data
+        session['text']=feedback.text.data
+        return redirect(url_for('success'))
+    return render_template('feedbackform.html',feedback=feedback)
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
 
 @app.route('/')
 def index():
@@ -21,19 +50,7 @@ def index():
 
 @app.route('/maths')
 def calculator():
-    #declare two variables
-    #perform add,sub,mul,div
-    #create index.html file 
-    # pass add,sub,mul,div variables to front-end
-    #display them using render_template 
-    
-    
-    return """<h1>calculator</h1>
-    """+ str(num1+num2) +"""
-    """+ str(num1-num2) +"""
-    """+ str(num1*num2) +"""
-    """+ str(num1/num2) +"""
-    """
+   pass
 
 @app.route('/maths/<float:num>')
 def maths(num):
@@ -65,11 +82,6 @@ def contact():
 def dashboard():
     data=['$10,000','$120,000','75%','12']
     return render_template('admin/index.html',data=data)
-
-
-@app.route('/login')
-def contact():
-    return render_template('contact.html')
 
 
 if __name__ == '__main__':
